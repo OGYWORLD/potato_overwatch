c1dd8aacd8c993761c53768ad25c7d16bb3ff5e5
OGY <ogy1004@naver.com>
2021-11-23 02:34:25 +0900

    해상도 fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -41,49 +41,57 @@ const zarya_content = (
 function Header() {
     return (
         <>
-        <div style = {{backgroundColor: '#222222', width: '100%', height: '430px', paddingRight: "15%"}}>
-        <div style = {{marginLeft: "20%"}}>
+        <div style = {{backgroundColor: '#222222', width: '100%', height: '430px'}}>
+        <img src = {bg} style = {{marginLeft: "36%", position:"absolute", opacity: "0.2"}}/>
 
+
+        <a style={{marginLeft: "17%"}}>
         <b style = {{fontSize: "65px", color: "white", marginLeft:"360px", marginTop:"160px", position:"absolute", float: "left", zIndex: "1"}} className = "title">OverWatch</b>
         <b style = {{fontSize: "65px", color: "white", marginLeft:"360px", marginTop:"240px", position:"absolute", float: "left", zIndex: "1"}} className = "title">전적 분석 사이트</b>
             <img src = {logo} style = {{width: '350px', height: '280px', marginTop: "98px", float: "left", position:"absolute"}}/>
-            <img src = {bg} style = {{marginLeft: "480px", position:"absolute", opacity: "0.2"}}/>
-            <b style = {{fontSize: "40px", color: "#D18515", marginLeft:"900px", marginTop:"350px", float: "left", position:"absolute", fontFamily: "koverwatch"}} className = "text-color">Best Hero</b>
 
             <Space direction="vertical">
-            <Search placeholder="닉네임#배틀태그" onSearch={onSearch} style={{ width: "260px", position:"absolute", marginLeft:"10px", marginTop:"350px", }} />
+            <Search placeholder="닉네임#배틀태그" onSearch={onSearch} style={{ width: "360px", position:"absolute", marginLeft:"10px", marginTop:"5px", }} />
             </Space>
 
             <Button icon={<img src={battlent} style = {{width: "30px", height: "30px", marginLeft: "140px", position:"absolute", }} />} href="https://kr.battle.net/login/ko/?ref=https://kr.battle.net/support/ko/&app=support"
-            style = {{width: "200px", position:"absolute", marginLeft:"286px", marginTop:"365px", background: "#017aff"}}>
+            style = {{width: "200px", position:"absolute", marginLeft:"10px", marginTop:"365px", background: "#017aff"}}>
             <b style = {{marginRight: "25px", color: "white",}}>내 배틀태그 확인하기</b>
             </Button>
 
             <Button icon={<img src={ov} style = {{width: "25px", height: "25px", marginLeft: "152px", marginTop: "3px",position:"absolute", }} />} href="https://playoverwatch.com/ko-kr/"
-            style = {{width: "200px", position:"absolute", marginLeft:"500px", marginTop:"365px", background: "white"}}>
+            style = {{width: "200px", position:"absolute", marginLeft:"250px", marginTop:"365px", background: "white"}}>
             <b style = {{marginRight: "25px", color: "#222222"}}>오버워치 홈페이지 접속</b>
             </Button>
+            </a>
 
-            <Popover content={zarya_content} title="자리야(Zarya) / 돌격(Tanker)">
-            <img src = {zarya_icon} style = {{position:"absolute", marginLeft:"1040px", marginTop:"340px", float: "left", height: "70px"}} />
-            </Popover>
+            <a style={{marginLeft: "35%"}}>
 
-            <Popover content={mercy_content} title="메르시(Mercy) / 지원(Healer)">
-            <img src = {mercy_icon} style = {{position:"absolute", marginLeft:"1120px", marginTop:"340px", float: "left", height: "70px"}} />
-            </Popover>
+            <b style = {{fontSize: "40px", color: "#D18515", marginLeft:"0px", marginTop:"350px", float: "left", position:"absolute", fontFamily: "koverwatch"}} className = "text-color">Best Hero</b>
+                <Popover content={zarya_content} title="자리야(Zarya) / 돌격(Tanker)">
+                <img src = {zarya_icon} style = {{position:"absolute", marginLeft:"140px", marginTop:"340px", float: "left", height: "70px"}} />
+                </Popover>
+    
+                <Popover content={mercy_content} title="메르시(Mercy) / 지원(Healer)">
+                <img src = {mercy_icon} style = {{position:"absolute", marginLeft:"220px", marginTop:"340px", float: "left", height: "70px"}} />
+                </Popover>
+    
+                <Popover content={reaper_content} title="리퍼(reaper) / 공격(Dealer)">
+                <img src = {reaper_icon} style = {{position:"absolute", marginLeft:"300px", marginTop:"340px", float: "left", height: "75px"}} />
+                </Popover>
+                
+                <b style = {{fontSize: "20px", color: "#D18515", marginLeft:"130px", marginTop:"170px", position:"absolute", float: "left"}} className = "title">닉네임 #배틀태그</b>
+                <b style = {{fontSize: "60px", color: "white", marginLeft:"130px", marginTop:"200px", position:"absolute", float: "left"}} className = "text-stroke">누룽지#12345
+                <hr style = {{height: "4px", width: "250px", marginLeft: "0px", marginTop: "0px", backgroundColor: "#666A7B", position:"absolute"}}/>
+                </b>
+                <img src = {mercy} style = {{marginLeft:"400px", marginTop:"180px", position:"absolute", width: "170px"}}/>
 
-            <Popover content={reaper_content} title="리퍼(reaper) / 공격(Dealer)">
-            <img src = {reaper_icon} style = {{position:"absolute", marginLeft:"1198px", marginTop:"340px", float: "left", height: "75px"}} />
-            </Popover>
-            
-            <b style = {{fontSize: "20px", color: "#D18515", marginLeft:"1030px", marginTop:"170px", position:"absolute", float: "left"}} className = "title">닉네임 #배틀태그</b>
-            <b style = {{fontSize: "60px", color: "white", marginLeft:"1030px", marginTop:"200px", position:"absolute", float: "left"}} className = "text-stroke">누룽지#12345
-            </b>
-            <hr style = {{height: "4px", width: "250px", marginLeft: "1030px", marginTop: "280px", backgroundColor: "#666A7B", position:"absolute"}}/>
-            <img src = {mercy} style = {{marginLeft:"1300px", marginTop:"180px", position:"absolute", width: "170px"}}/>
+            </a>
 
-        </div>
-        </div>
+
+
+             </div>
+
         <div style = {{backgroundColor: '#F8E064', width: '100%', height: '20px'}}></div>
         <div style = {{backgroundColor: '#D18515', width: '100%', height: '30px'}}></div>
         <br/>
